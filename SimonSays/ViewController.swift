130b84c40fbebf4303b4d3b0aebde516712a6fac
Marcus Norona <marcusjeremynorona@gmail.com>
2017-07-07 18:24:23 -0700

    gyroscope Simon says

--- SimonSays/ViewController.swift
+++ SimonSays/ViewController.swift
@@ -7,12 +7,78 @@
 //
 
 import UIKit
+import CoreMotion
+import Foundation
 
 class ViewController: UIViewController {
+    var tilt = [0,0]
+    @IBOutlet weak var redBox: UIView!
+    var motionManager = CMMotionManager()
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+    }
+    
+    override func viewDidAppear(_ animated: Bool) {
+        motionManager.gyroUpdateInterval = 0.3
+
+        
+        var value = 1
+        motionManager.startGyroUpdates(to: OperationQueue.current!) { (data, error) in
+            if let myData = data {
+                
+                if myData.rotationRate.x > 2{//forward motion
+                    if value == 1 {
+                        self.tilt[0] = value
+                        value = 2
+                    } else {
+                        self.tilt[0] = value
+                        value = 1
+                        print ("You tilted forward")
+                        print("current tilt: \(self.tilt)")
+                    }
+
+                }
+                else if myData.rotationRate.x < -2 { //backward motion
+                    if value == 1 {
+                        
+                        self.tilt[1] = value
+                        value = 2
+                    } else {
+                        self.tilt[1] = value
+                        value = 1
+                        print ("You tilted back")
+                        print("current tilt: \(self.tilt)")
+                    }
+                }
+                
+                print(self.tilt)
+            }
+        }
+        
+        let helloWorldTimer = Timer.scheduledTimer(timeInterval: 0.6, target: self, selector: #selector(self.tiltEval), userInfo: nil, repeats: true)
+        
+        
+        
+    }
+    
+    func sayHello()
+    {
+        NSLog("hello World")
+    }
+    
+    
+    func tiltEval(){
+        if self.tilt[0] == 1 && self.tilt[1]  == 2 {
+            print(2)
+        } else if self.tilt[0] == 2 && self.tilt[1] == 1 {
+            print(1)
+        } else {
+            print(3)
+        }
+        self.tilt = [0,0]
+        
     }
 
     override func didReceiveMemoryWarning() {
@@ -20,6 +86,6 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
-
+    
 }
 
